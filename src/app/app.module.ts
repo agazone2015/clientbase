4d096f0298a4271abaddc278ed04915dc3de857c
agazone2015 <agazone@hotmail.com>
2017-09-20 16:25:10 +1000

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './comoponents/dashboard/dashboard.component';
+import { ClientsComponent } from './comoponents/clients/clients.component';
+import { ClientDetailsComponent } from './comoponents/client-details/client-details.component';
+import { AddClientComponent } from './comoponents/add-client/add-client.component';
+import { EditClientComponent } from './comoponents/edit-client/edit-client.component';
+import { NavbarComponent } from './comoponents/navbar/navbar.component';
+import { SidebarComponent } from './comoponents/sidebar/sidebar.component';
+import { LoginComponent } from './comoponents/login/login.component';
+import { RegisterComponent } from './comoponents/register/register.component';
+import { SettingsComponent } from './comoponents/settings/settings.component';
+import { PageNotFoundComponent } from './comoponents/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
